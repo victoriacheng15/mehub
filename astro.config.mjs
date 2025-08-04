c970402159ee3b1926e143e968e89bb45e35f177
Victoria Cheng <victoria.cheng15@gmail.com>
2025-08-04 15:48:55 -0400

    edit config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,15 @@
 import { defineConfig } from "astro/config";
 import { siteConfig } from "./src/config";
-import rehypeMermaid from 'rehype-mermaid';
-
-import tailwindcss from "@tailwindcss/vite";
-
+import { visit } from 'unist-util-visit';
 import expressiveCode from "astro-expressive-code";
+import rehypeMermaid from 'rehype-mermaid';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	site: siteConfig.site,
 	integrations: [expressiveCode()],
 	markdown: {
-		syntaxHighlight: {
-			type: "shiki",
-		},
-		rehypePlugins: [
-			[
-				rehypeMermaid,
-				{
-					mermaidConfig: {
-						theme: "default",
-					},
-				},
-			],
-		],
+		rehypePlugins: [rehypeMermaid],
 	},
 	vite: {
 		plugins: [tailwindcss()],
