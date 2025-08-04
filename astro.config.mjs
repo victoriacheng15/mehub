edbe05439826975b8c36c9b4c1bce2425474b925
Victoria Cheng <victoria.cheng15@gmail.com>
2025-08-04 15:28:07 -0400

    add mermaid for markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,28 @@
 import { defineConfig } from "astro/config";
 import { siteConfig } from "./src/config";
+import rehypeMermaid from 'rehype-mermaid';
 
 import tailwindcss from "@tailwindcss/vite";
 
+import expressiveCode from "astro-expressive-code";
+
 export default defineConfig({
 	site: siteConfig.site,
-	integrations: [],
+	integrations: [expressiveCode()],
 	markdown: {
-		shikiConfig: {
-			theme: "laserwave",
+		syntaxHighlight: {
+			type: "shiki",
 		},
+		rehypePlugins: [
+			[
+				rehypeMermaid,
+				{
+					mermaidConfig: {
+						theme: "default",
+					},
+				},
+			],
+		],
 	},
 	vite: {
 		plugins: [tailwindcss()],
