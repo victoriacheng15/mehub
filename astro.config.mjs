4bac9f90b34540e27e8862fad7e345f20cbcb812
Victoria Cheng <victoria.cheng15@gmail.com>
2025-08-04 15:50:45 -0400

    add mermaid for markdown (#340)
    * add mermaid for markdown
    
    * chore(deps): bump astro from 5.12.4 to 5.12.8 (#337)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps): bump h3 from 1.15.3 to 1.15.4 (#335)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * edit config
    
    ---------
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
 import { siteConfig } from "./src/config";
-
+import { visit } from 'unist-util-visit';
+import expressiveCode from "astro-expressive-code";
+import rehypeMermaid from 'rehype-mermaid';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	site: siteConfig.site,
-	integrations: [],
+	integrations: [expressiveCode()],
 	markdown: {
-		shikiConfig: {
-			theme: "laserwave",
-		},
+		rehypePlugins: [rehypeMermaid],
 	},
 	vite: {
 		plugins: [tailwindcss()],
