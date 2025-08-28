adb973b41d6ce625ac17eda80962ffa02c8c512c
Victoria Cheng <victoria.cheng15@gmail.com>
2025-08-28 14:30:40 -0600

    ci: update deploy production yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import { siteConfig } from "./src/config";
-import { visit } from 'unist-util-visit';
 import expressiveCode from "astro-expressive-code";
 import rehypeMermaid from 'rehype-mermaid';
 import tailwindcss from "@tailwindcss/vite";
