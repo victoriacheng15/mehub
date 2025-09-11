be5a70e33cf2b423d79f75654e986547ef45ad84
Victoria Cheng <victoria.cheng15@gmail.com>
2025-09-11 11:11:59 -0600

    update front page description

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ import type { PostFilter } from "./utils/posts";
 
 export interface SiteConfig {
 	title: string;
+	name: string;
 	slogan: string;
 	description?: string;
 	site: string;
@@ -12,9 +13,10 @@ export interface SiteConfig {
 export const siteConfig: SiteConfig = {
 	site: "https://victoriacheng15.vercel.app/",
 	title: "Victoria's Tech Hub",
+	name: "Victoria Cheng",
 	slogan: "Navigating the endless world of technology.",
 	description:
-		"Welcome to my tech hub! Explore my blogs, projects, and skills as I navigate the ever-evolving world of technology. Join me on this journey of discovery and innovation.",
+		"Welcome to my tech hub! I’m a Software Development student at SAIT, sharing blogs, projects, and lessons along the way. After completing my internship at Shopify, I’m continuing to build skills and prepare for the next chapter of my journey in technology.",
 	rss: true,
 	homepage: {
 		maxPosts: 5,
